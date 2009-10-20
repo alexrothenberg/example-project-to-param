026cb15549ca6be6777078e65b561801076247f2
Alex Rothenberg <alex@alexrothenberg.com>
2009-10-20 10:30:47 -0400

    fixed so find matches to_param

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,4 +3,15 @@ class City < ActiveRecord::Base
   def to_param
     self.permanent_name
   end
+  
+  private
+  def City.find_one(id, options)
+    return super if id.is_a? Numeric
+    find_by_permanent_name(id)
+  end
+
+  def City.find_some(ids, options)
+    return super if ids.all? {|id| id.is_a? Numeric }
+    find_all_by_permanent_name(ids)
+  end
 end
